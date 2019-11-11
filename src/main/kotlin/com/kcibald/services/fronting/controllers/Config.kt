645a04fd186d5fb392f8840e527264e0d15e7856
wusatosi <26424577+wusatosi@users.noreply.github.com>
2019-11-11 14:45:13 +0800

    Finished logout api

--- src/main/kotlin/com/kcibald/services/fronting/controllers/Config.kt
+++ src/main/kotlin/com/kcibald/services/fronting/controllers/Config.kt
@@ -6,5 +6,6 @@ object Config: ConfigSpec() {
     object Authentication: ConfigSpec("authentication") {
         val CookieKey by optional("_kb_aut", "cookie_key")
         val RecaptchaThreshold by optional(1.0, "login_recaptcha_threshold", "should be less than 1")
+        val RedirectURLWhenLogout by optional("/login", "redirect_url_when_logout")
     }
 }
--- src/main/kotlin/com/kcibald/services/fronting/controllers/common/Logout.kt
+++ src/main/kotlin/com/kcibald/services/fronting/controllers/common/Logout.kt
@@ -1,24 +1,50 @@
 package com.kcibald.services.fronting.controllers.common
 
+import com.kcibald.services.fronting.ContentTypes
+import com.kcibald.services.fronting.controllers.Config.Authentication
 import com.kcibald.services.fronting.coreHandler
-import com.kcibald.services.fronting.objs.entries.APIEntry
+import com.kcibald.services.fronting.objs.entries.FancyEntry
+import com.kcibald.services.fronting.objs.responses.EmptyResponse
+import com.kcibald.services.fronting.objs.responses.RedirectResponse
 import com.kcibald.services.fronting.objs.responses.Response
 import com.uchuhimo.konf.Config
 import io.vertx.core.Vertx
+import io.vertx.ext.web.Cookie
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 
-object Logout : APIEntry {
+object Logout : FancyEntry {
 
     override fun routeAPIEndpoint(router: Router, vertx: Vertx, configSource: Config) {
+        val cookieKey = configSource[Authentication.CookieKey]
+
         router
             .post("/logout")
-            .coreHandler(Logout::logoutAPI)
+            .coreHandler {
+                clearCookie(cookieKey, it)
+                EmptyResponse
+            }
     }
 
-    private fun logoutAPI(context: RoutingContext): Response {
-        context
-        TODO()
+    override fun routeHTMLContent(router: Router, vertx: Vertx, configSource: Config) {
+        val cookieKey = configSource[Authentication.CookieKey]
+        val redirectURL = configSource[Authentication.RedirectURLWhenLogout]
+
+        router
+            .get("/logout")
+            .produces(ContentTypes.HTML)
+            .coreHandler {
+                clearCookie(cookieKey, it)
+                RedirectResponse(redirectURL)
+            }
+    }
+
+    private fun clearCookie(cookieKey: String, context: RoutingContext) {
+        val cookie = Cookie.cookie(cookieKey, "_")
+        cookie.setMaxAge(-1)
+        cookie.setSecure(true)
+        cookie.domain = "kcibald.com"
+        context.addCookie(cookie)
     }
 
 }
