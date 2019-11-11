3b65689da79da0a798f3e8badf2fcd617b958606
wusatosi <26424577+wusatosi@users.noreply.github.com>
2019-11-11 13:19:28 +0800

    Progress on Login Endpoint Handler

--- src/main/kotlin/com/kcibald/services/fronting/controllers/common/Login.kt
+++ src/main/kotlin/com/kcibald/services/fronting/controllers/common/Login.kt
@@ -5,23 +5,35 @@ import com.kcibald.services.fronting.objs.entries.FancyEntry
 import com.kcibald.services.fronting.objs.entries.Path
 import com.kcibald.services.fronting.objs.entries.UnsafeHTMLContentEntry
 import com.kcibald.services.fronting.objs.responses.EmptyResponse
+import com.kcibald.services.fronting.objs.responses.InternalErrorResponse
 import com.kcibald.services.fronting.objs.responses.Response
-import com.wusatosi.recaptcha.v3.RecaptchaV3Client
+import com.kcibald.services.user.AuthenticationClient
+import com.kcibald.services.user.AuthenticationResult
+import io.vertx.core.Vertx
 import io.vertx.core.http.HttpServerResponse
+import io.vertx.ext.auth.jwt.JWTAuth
+import io.vertx.ext.auth.jwt.JWTAuthOptions
+import io.vertx.ext.web.Cookie
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
-import io.vertx.ext.web.handler.CookieHandler
 import io.vertx.kotlin.core.json.get
 import io.vertx.kotlin.core.json.json
+import io.vertx.kotlin.core.json.jsonObjectOf
 import io.vertx.kotlin.core.json.obj
+import io.vertx.kotlin.ext.jwt.jwtOptionsOf
 import org.apache.commons.validator.routines.EmailValidator
 import org.slf4j.LoggerFactory
 import java.io.IOException
+import java.util.concurrent.TimeUnit
 import java.util.regex.Pattern
 
 object Login : UnsafeHTMLContentEntry(), FancyEntry {
     private val logger = LoggerFactory.getLogger(Login::class.java)
+
+    //    should be imported from config or some sort
     private val recaptchaScoreThreshold: Double = 0.7
+    private const val COOKIE_KEY = ""
+    private val authenticationClient = AuthenticationClient.createDefault(Vertx.vertx())
 
     override fun routeAPIEndpoint(router: Router) {
         router
@@ -32,6 +44,7 @@ object Login : UnsafeHTMLContentEntry(), FancyEntry {
 
     private val emailValidator = EmailValidator.getInstance()
     private val passwordPattern = Pattern.compile("^(?=.*?[a-z])(?=.*?[0-9]).{8,20}\$")
+    private val jwtAuthProvider = JWTAuth.create(Vertx.vertx(), JWTAuthOptions())
 
     private suspend fun loginAPI(context: RoutingContext): Response {
         val requestObj = context.jsonObject
@@ -44,7 +57,7 @@ object Login : UnsafeHTMLContentEntry(), FancyEntry {
 //        pre-process check
         if (!emailValidator.isValid(account) || !passwordPattern.matcher(password).matches()) {
             logger.debug("account: $account login attempt failed on pre-check")
-            return AuthorizationFailureResponses.PASSWORD_OR_ACCOUNT_EMAIL_INCORRECT
+            return StandardAuthorizationFailureResponses.PASSWORD_OR_ACCOUNT_EMAIL_INCORRECT
         }
 
 //        might want to make recaptcha verification parell to account information processing
@@ -59,7 +72,7 @@ object Login : UnsafeHTMLContentEntry(), FancyEntry {
                         "logging attempt by account $account denied because recaptcha score is under threshold" +
                                 "($recaptchaScoreThreshold > $score)"
                     )
-                    return AuthorizationFailureResponses.CAPTCHA_VERIFICATION_FAILED
+                    return StandardAuthorizationFailureResponses.CAPTCHA_VERIFICATION_FAILED
                 } else {
                     break
                 }
@@ -78,14 +91,63 @@ object Login : UnsafeHTMLContentEntry(), FancyEntry {
             }
         }
 
-//        TODO: user verification magic
-//        TODO: cookie magic
+        when (val verification = authenticationClient.verifyCredential(account, password)) {
+            is AuthenticationResult.InvalidCredential, is AuthenticationResult.UserNotFound ->
+                return StandardAuthorizationFailureResponses.PASSWORD_OR_ACCOUNT_EMAIL_INCORRECT
+            is AuthenticationResult.Banned ->
+                return bannedFailure(verification)
+            is AuthenticationResult.Success -> {
+                val expireInMinutes = if (unsafeLogin) 5 else TimeUnit.DAYS.toMinutes(30).toInt()
 
-        logger.debug("account: $account successfully logged in")
-        return EmptyResponse
+                val user = verification.user
+                val claims = jsonObjectOf(
+                    "username" to user.userName,
+                    "user_avatar" to user.avatar
+                )
+
+                val token = jwtAuthProvider.generateToken(
+                    claims, jwtOptionsOf(
+                        audience = listOf(user.userName, account),
+                        expiresInMinutes = expireInMinutes,
+                        issuer = "kcibald-frontend"
+                    )
+                )
+                val cookie = Cookie.cookie(COOKIE_KEY, token)
+                cookie.setSecure(true)
+                cookie.setMaxAge(TimeUnit.MINUTES.toSeconds(expireInMinutes.toLong()))
+                cookie.domain = "kcibald.com"
+                context.addCookie(cookie)
+                logger.debug("account: $account successfully logged in")
+                return EmptyResponse
+            }
+            is AuthenticationResult.SystemError -> {
+                logger.warn("received system error from upstream service: authentication, message ${verification.message}")
+                return InternalErrorResponse
+            }
+        }
     }
 
-    private enum class AuthorizationFailureResponses(val type: String) : Response {
+    private fun bannedFailure(result: AuthenticationResult.Banned): Response = object : Response {
+        override fun apply(response: HttpServerResponse) {
+            val timeToUnban = if (result.duration < 0) -1 else result.timeBanned + result.duration
+            val body = json {
+                obj(
+                    "success" to false,
+                    "type" to "USER_BANNED",
+                    "banned_type" to obj(
+                        "message" to result.message,
+                        "time_of_unban" to timeToUnban
+                    )
+                )
+            }
+            response
+                .setStatusCode(403)
+                .putHeader("WWW-Authenticate", "FormBased")
+                .end(body)
+        }
+    }
+
+    private enum class StandardAuthorizationFailureResponses(val type: String) : Response {
         PASSWORD_OR_ACCOUNT_EMAIL_INCORRECT("PASSWORD_OR_ACCOUNT_EMAIL_INCORRECT"),
         CAPTCHA_VERIFICATION_FAILED("CAPTCHA_VERIFICATION_FAILED");
 
