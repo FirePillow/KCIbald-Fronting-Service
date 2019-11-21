0454003de9d44f4bf56ca1469a5f11d70bd5cfbd
wusatosi <26424577+wusatosi@users.noreply.github.com>
2019-11-21 18:24:34 +0800

    Mounted CommonAPIRoutes

--- src/main/kotlin/com/kcibald/services/fronting/FrontingServiceVerticle.kt
+++ src/main/kotlin/com/kcibald/services/fronting/FrontingServiceVerticle.kt
@@ -1,16 +1,27 @@
 package com.kcibald.services.fronting
 
+import com.kcibald.services.fronting.controllers.common.CommonAPIRouter
+import com.uchuhimo.konf.Config
 import io.vertx.ext.web.Router
-import io.vertx.ext.web.RoutingContext
-import io.vertx.ext.web.handler.VirtualHostHandler
+import io.vertx.kotlin.core.http.listenAwait
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 
-object FrontingServiceVerticle: CoroutineVerticle() {
+object FrontingServiceVerticle : CoroutineVerticle() {
 
     override suspend fun start() {
-        val router = Router.router(vertx)
+        val config = Config { addSpec(com.kcibald.services.fronting.controllers.Config) }
+            .from.json.resource("config.json")
 
-        vertx.createHttpServer().requestHandler(router::handle).listen()
+        val router = Router.router(vertx)
+        CommonAPIRouter.routeAPIEndpoint(router, vertx, config)
+        CommonAPIRouter.routeHTMLContent(router, vertx, config)
+
+        val port = config[com.kcibald.services.fronting.controllers.Config.httpPort]
+        vertx
+            .createHttpServer()
+            .requestHandler(router::handle)
+            .listenAwait(port)
+
         super.start()
     }
 
--- src/main/kotlin/com/kcibald/services/fronting/controllers/Config.kt
+++ src/main/kotlin/com/kcibald/services/fronting/controllers/Config.kt
@@ -3,6 +3,7 @@ package com.kcibald.services.fronting.controllers
 import com.uchuhimo.konf.ConfigSpec
 
 object Config: ConfigSpec() {
+    val httpPort by optional(8080, "http_port")
     object Authentication: ConfigSpec("authentication") {
         val CookieKey by optional("_kb_aut", "cookie_key")
         val RecaptchaThreshold by optional(0.7, "login_recaptcha_threshold", "should be less than 1")
